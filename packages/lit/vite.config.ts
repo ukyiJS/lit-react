71f4cb5560d26b3c88609e4a9f47ef01b8951c45
ukyi <ukyi.js@gmail.com>
2024-02-02 19:56:05 +0900

    ✨ lib build 추가

--- packages/lit/vite.config.ts
+++ packages/lit/vite.config.ts
@@ -1,5 +1,6 @@
 import { URL, fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
+import dtsPlugin from 'vite-plugin-dts';
 
 export default defineConfig({
   resolve: {
@@ -7,4 +8,13 @@ export default defineConfig({
       src: fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    lib: {
+      name: 'lit-element',
+      entry: 'src/index.ts',
+      formats: ['es'],
+      fileName: 'index',
+    },
+  },
+  plugins: [dtsPlugin()],
 });
