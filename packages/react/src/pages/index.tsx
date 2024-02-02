6990241fe526473fde2105e57df0beb9588e6f67
ukyi <ukyi.js@gmail.com>
2024-02-02 19:56:58 +0900

    ✨ LitReactToastComponent, LitReactButtonComponent 추가 - lit element -> react component 변환

--- packages/react/src/pages/index.tsx
+++ packages/react/src/pages/index.tsx
@@ -1,9 +1,31 @@
+import { createComponent } from '@lit/react';
+import React, { useRef } from 'react';
 import HelloWorld from 'src/components/HelloWorld';
+import { LitReactButton, LitReactToast } from '@lit-react-example/lit';
+
+const LitReactToastComponent = createComponent({
+  tagName: 'lit-react-toast',
+  elementClass: LitReactToast,
+  react: React,
+});
+
+const LitReactButtonComponent = createComponent({
+  tagName: 'lit-react-button',
+  elementClass: LitReactButton,
+  react: React,
+});
 
 export default function Index() {
+  const toast = useRef<LitReactToast>(null);
+  const handleButtonClick = () => {
+    toast.current?.show('Hello world!');
+  };
+
   return (
     <main>
       <HelloWorld />
+      <LitReactToastComponent ref={toast} />
+      <LitReactButtonComponent variant="primary" onClick={handleButtonClick}>toast</LitReactButtonComponent>
     </main>
   );
 }
